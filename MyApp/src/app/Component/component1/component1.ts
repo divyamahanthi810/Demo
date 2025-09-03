9ee53d72d14aaf1052c949ad7af67c74d537e270
Dileepkumar74 <dileepkumarrothu1441@gmail.com>
2025-09-03 08:11:12 +0530

    Dileep

---
+++ MyApp/src/app/Component/component1/component1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component1',
+  imports: [],
+  templateUrl: './component1.html',
+  styleUrl: './component1.css'
+})
+export class Component1 {
+
+}
