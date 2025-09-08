fff39eb92c8c63a612abc7b3742f83c90c750063
Mahanthi Divya <divyamahanthi810@gmail.com>
2025-09-08 23:09:05 +0530

    changed to animation

--- MyApp/src/app/Component/toolbar/toolbar.ts
+++ MyApp/src/app/Component/toolbar/toolbar.ts
@@ -12,39 +12,89 @@ import { FormControl, FormGroup, ReactiveFormsModule,Validators} from '@angular/
 import { RouterOutlet } from '@angular/router';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import { ViewChild } from '@angular/core';
+import { AfterViewInit, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 @Component({
   selector: 'app-toolbar',
-  imports: [ RouterOutlet ,MatSidenavModule,MatSnackBarModule,ReactiveFormsModule,CommonModule,MatIconModule,MatGridListModule,MatCardModule,MatToolbarModule,RouterLink,RouterLinkActive,MatButtonModule],
+  imports: [RouterOutlet ,MatSidenavModule,MatSnackBarModule,ReactiveFormsModule,CommonModule,MatIconModule,MatGridListModule,MatCardModule,MatToolbarModule,RouterLink,RouterLinkActive,MatButtonModule],
   templateUrl: './toolbar.html',
   styleUrl: './toolbar.css'
 })
 export class Toolbar {
+  scrollTo(sectionId: string) {
+  const element = document.getElementById(sectionId);
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
+}
+ @ViewChildren('animatedSection', { read: ElementRef }) animatedSections!: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }else {
+          entry.target.classList.remove('show'); // Remove when out of view
+        }
+      });
+    }, { threshold: 0.2 });
+
+    this.animatedSections.forEach(section => observer.observe(section.nativeElement));
+  }
+  cards = [
+    {
+      title: 'Frontend Developer',
+      image: '/frontend.jpeg',
+      description:'`A front-end developer builds the parts of websites and applications that users see and interact with directly,using core languages like HTML, CSS, and JavaScript to create the visual interface.`',
+    },
+    {
+      title: 'Full Stack Developer',
+      image: '/Full stack.png',
+      description: '`A front-end developer builds the parts of websites and applications that users see and interact with directly,using core languages like HTML, CSS, and JavaScript to create the visual interface.`',
+    },
+    {
+      title: 'Backend Developer',      
+      image: '/Backend.png',  
+      description: '`A backend developer builds and maintains the server-side of websites and applications,focusing on the parts users dont see A backend developer builds and maintains the server-side of websites and applications,focusing on the parts users dont see.`'
+    },
+    {
+      title: 'service4',
+      image: '/Backend.png',  
+      description: '`A backend developer builds and maintains the server-side of websites and applications,focusing on the parts users dont see.`'
+    }                                                                                                                                           
+
+  ];
   @ViewChild('sidenav') sidenav!: MatSidenav;
 
   toggleMenu() {
     this.sidenav.toggle();
   }
-   backend=`A backend developer builds and maintains the server-side of websites and applications,
-   focusing on the parts users don't see.`;
+   currentIndex = 0;
+  visibleCards:any[] = [];
 
-  frontend=`A front-end developer builds the parts of websites and applications that users see and interact with directly,
-   using core languages like HTML, CSS, and JavaScript to create the visual interface.`;
+  ngOnInit() {
+    this.updateVisibleCards();
+  }
 
-  currentPosition = 0;
-  cardWidth = 320; 
+ updateVisibleCards() {
+  this.visibleCards = this.cards.slice(this.currentIndex, this.currentIndex + 3);
+
+}
 
   nextSlide() {
-    if (this.currentPosition > -(this.cardWidth * 2)) { 
-      this.currentPosition -= this.cardWidth;
+    if (this.currentIndex + 3 < this.cards.length) {
+      this.currentIndex++;
+      this.updateVisibleCards();
     }
   }
 
   prevSlide() {
-    if (this.currentPosition < 0) {
-      this.currentPosition += this.cardWidth;
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+      this.updateVisibleCards();
     }
-}
+  }
 
   teamMembers = [
     {
